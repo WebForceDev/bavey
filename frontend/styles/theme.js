bab68fd309663fec1073255ddcbab52e096f644e
SergeyAverin <pretty.street.003@gmail.com>
2022-10-09 16:34:06 +0300

    feat: added theme.js

---
+++ frontend/styles/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+    color: {
+        white: '#ffffff',
+        bg: '#1D1D1D',
+        grey: '#272727',
+        black: '#111111',
+        grean: '#00BC5E'
+    }
+};
+
+export default theme;
