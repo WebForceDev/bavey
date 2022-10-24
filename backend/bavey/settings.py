2c0a255c35a837130a97a3cd0b5024bc24dd8b5a
SergeyAverin <pretty.street.003@gmail.com>
2022-10-24 08:03:00 +0300

    feat: added user model
    used AbstractUser to added new field in django user model

--- backend/bavey/settings.py
+++ backend/bavey/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'main.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    description = models.TextField(
+        'Description in profile header',
+        max_length=30,
+        null=True,
+        blank=True
+    )
+    avatar = models.ImageField(
+        upload_to='user/avatars',
+        null=True,
+        blank=True
+    )
+    header_image = models.ImageField(
+        upload_to='user/header_image',
+        null=True,
+        blank=True
+    )
