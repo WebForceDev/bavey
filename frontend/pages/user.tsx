924ccae1549aad0ef8429b58dd7dc82def17ad44
SergeyAverin <pretty.street.003@gmail.com>
2022-12-19 12:32:46 +0300

    feat: added margin styled component
    This component add margin for child component

--- frontend/pages/user.tsx
+++ frontend/pages/user.tsx
@@ -6,40 +6,48 @@ import Board from '../components/Board/Board';
 import UserMiniTitle from '../components/UserMiniTitle/UserMiniTitle';
 import BaseLayout from '../components/BaseLayout/BaseLayout';
 import ContentGrid from '../styles/components/ContentGrid';
+import Margin from '../styles/components/Margin';
 
 
 const Home: NextPage = () => {
   return (
     <BaseLayout>
-      <ContentGrid>
-          <div>
-            <Board>
-                <UserMiniTitle />
-                <UserMiniTitle />
-                <UserMiniTitle />
-            </Board>
-            <Board>
-                <UserMiniTitle />
-                <UserMiniTitle />
-                <UserMiniTitle />
-            </Board>
-          </div>
+      <Margin mg='59px 0 0 0'>
+        <ContentGrid>
+            <div>
+              <Margin mg='0 0 15px 0'>
+                <Board>
+                    <UserMiniTitle />
+                    <UserMiniTitle />
+                    <UserMiniTitle />
+                </Board>
+              </Margin>
+              <Board>
+                  <UserMiniTitle />
+                  <UserMiniTitle />
+                  <UserMiniTitle />
+              </Board>
+            </div>
 
-          <div>
-            <Publication />
-            <Publication />
-            <Publication />
-            <Publication />
-          </div>
+            <div>
+                <Margin mg='0 0 30px 0'>
+                  <Publication />
+                </Margin>
+                <Margin mg='0 0 30px 0'>
+                  <Publication />
+                </Margin>
+              
+            </div>
 
-          <div>
-            <Board>
-                <UserMiniTitle />
-                <UserMiniTitle />
-                <UserMiniTitle />
-            </Board>
-          </div>
-      </ContentGrid>
+            <div>
+              <Board>
+                  <UserMiniTitle />
+                  <UserMiniTitle />
+                  <UserMiniTitle />
+              </Board>
+            </div>
+        </ContentGrid>
+      </Margin>
     </BaseLayout>
   )
 }
