15851b45c37acff5b090ef5d4c5853d5029a5a65
SergeyAverin <pretty.street.003@gmail.com>
2023-02-12 10:25:47 +0300

    feat: added profile page

---
+++ frontend/components/UserHeader/UserHeader.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Link from "next/link";
+
+import { ProfileHeaderStyled, ProfileHeaderBackground, ProfileTitle, UserName } from "./style";
+import UserTitle from "../UserTitle/UsetTitle";
+
+import WrapperStyled from "../../styles/components/Wrapper"
+import ContentGrid from "../../styles/components/ContentGrid";
+import FlexStyled from "../../styles/components/Flex";
+import Margin from "../../styles/components/Margin";
+import { IUser } from "../../types/user";
+
+
+interface IProfileHeaderProps {
+    user: IUser
+}
+
+const UserHeader: React.FC<IProfileHeaderProps> = ({ user }) => {
+    return (
+        <ProfileHeaderStyled>
+            <ProfileHeaderBackground />
+            <WrapperStyled>
+                <ContentGrid>
+                    <ProfileTitle>
+                        <UserTitle />
+                    </ProfileTitle>
+                    <FlexStyled justifyContent="space-between" alignItems="flex-start">
+                        <div>
+                            <UserName>
+                                { user.username }
+                            </UserName>
+                            <Link href={ user.slug }>{ user.slug }</Link>
+                            <Margin mg="25px 0 0 0">
+                                { user.description }
+                            </Margin>
+                        </div>
+                        <Margin mg="25px 0 0 0">
+                            2 часа назад
+                        </Margin>
+                    </FlexStyled>
+                </ContentGrid>
+            </WrapperStyled>
+        </ProfileHeaderStyled>        
+    )
+};
+
+export default UserHeader;
