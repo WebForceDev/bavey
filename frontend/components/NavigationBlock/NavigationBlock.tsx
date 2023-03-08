92b51f5dc991453ef37ccd38c97b47d3018ffef5
SergeyAverin <pretty.street.003@gmail.com>
2023-03-08 18:57:07 +0300

    feat: added navigation component

---
+++ frontend/components/NavigationBlock/NavigationBlock.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import { NavigationBlockStyled } from './style';
+import FlexStyled from '../../styles/components/Flex';
+import Margin from '../../styles/components/Margin';
+
+import Arrow from "../../public/arrow.svg";
+
+
+const NavigationBlock: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <NavigationBlockStyled
+            isOpen={isOpen}
+            onClick={() => setIsOpen((prev)=>(!prev))}
+        >
+
+        <FlexStyled alignItems='center' justifyContent='space-between' >
+            saved post
+            <Image src={Arrow} alt="arrow" />
+        </FlexStyled>
+        
+        { isOpen &&
+            <>
+                <Margin mg='10px 0 0 0'>
+                        <div>
+                            <Link href='/friends'>friends</Link>
+                        </div>
+                    </Margin>
+                    <Margin mg='10px 0 0 0'>
+                    <div>
+                        <Link href='/savedpost'>saved post</Link>
+                    </div>
+                </Margin>
+            </>
+        }   
+        </NavigationBlockStyled>
+    )
+};
+
+export default NavigationBlock;
